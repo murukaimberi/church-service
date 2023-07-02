297882df9638d3e1dd3c9304bc84fad574e8fc3b
Murukai Gumbo-Mberi <gmurukai@hotmail.com>
2023-07-02 12:20:23 +0200

    cleaning up the code

---
+++ docker/create-database.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA `church_authority` DEFAULT CHARACTER SET utf8;
+CREATE SCHEMA `church_blog` DEFAULT CHARACTER SET utf8;
+CREATE SCHEMA `church_registration` DEFAULT CHARACTER SET utf8;
